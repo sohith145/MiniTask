a3ff9d7a8ce46b3283f93338cc78b4eb0f2f113c
sohith <sohith145@gmail.com>
2026-09-20 10:35:33 +0530

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://mini-task-rho.vercel.app"],
     credentials: true,
   }),
 );
